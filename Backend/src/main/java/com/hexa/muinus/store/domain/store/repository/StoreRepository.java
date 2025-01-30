23821307d6e218e3032da87fd5e7f86593785c95
taemin323 <155932839+taemin323@users.noreply.github.com>
2025-01-30 20:38:11 +0900

    [FEAT] 쿠폰 발급 구현 (#8)

--- Backend/src/main/java/com/hexa/muinus/store/domain/store/repository/StoreRepository.java
+++ Backend/src/main/java/com/hexa/muinus/store/domain/store/repository/StoreRepository.java
@@ -2,6 +2,13 @@ package com.hexa.muinus.store.domain.store.repository;
 
 import com.hexa.muinus.store.domain.Store;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface StoreRepository extends JpaRepository<Store, Integer> {
+    // 가게 이름으로 store_no 조회
+    @Query("SELECT s.storeNo FROM Store s WHERE s.name = :storeName")
+    Optional<Integer> findStoreNoByName(@Param("storeName") String storeName);
 }
