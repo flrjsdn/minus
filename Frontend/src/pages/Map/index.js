c07e05cb35352f8fc7b490befd0e04190e8c96df
aseongjun99 <aseongjun99@gmail.com>
2025-02-10 21:29:41 +0900

    배포 테스트

--- Frontend/src/pages/Map/index.js
+++ Frontend/src/pages/Map/index.js
@@ -1,13 +1,13 @@
 import HeaderContainer from "../../components/HeaderContainer/HeaderContainer";
 import BottomNav from "../../components/BottomNav/BottomNav";
-import KakaoMap from "../../components/KakaoMap";
+// import KakaoMap from "../../components/KakaoMap";
 
 function Map() {
     return (
         <div>
             <HeaderContainer />
             <h2>지도 테스트</h2>
-            <KakaoMap/>
+            {/* <KakaoMap/> */}
 
             <BottomNav />
         </div>
