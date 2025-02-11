1544be76488a6620b0805e61d7b2d65a093f6e2f
이지희 <chaehee13@naver.com>
2025-02-12 07:33:12 +0900

    [BE/FEAT/#32] Redis 해시키 형태 변경

--- Batch/src/main/java/com/hexa/muinus/batch/common/RedisConfig.java
+++ Batch/src/main/java/com/hexa/muinus/batch/common/RedisConfig.java
@@ -1,5 +1,9 @@
 package com.hexa.muinus.batch.common;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -15,12 +19,20 @@ public class RedisConfig {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(connectionFactory);
 
+        // Key 및 HashKey 직렬화
         template.setKeySerializer(new StringRedisSerializer());
         template.setHashKeySerializer(new StringRedisSerializer());
 
-        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
-        template.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
+        // ObjectMapper 설정
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        objectMapper.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL, JsonTypeInfo.As.PROPERTY);
 
+        GenericJackson2JsonRedisSerializer serializer = new GenericJackson2JsonRedisSerializer(objectMapper);
+        template.setValueSerializer(serializer);
+        template.setHashValueSerializer(serializer);
+
+        template.afterPropertiesSet();
         return template;
     }
 }
--- Batch/src/main/java/com/hexa/muinus/temp/RedisTestDataLoader.java
+++ Batch/src/main/java/com/hexa/muinus/temp/RedisTestDataLoader.java
@@ -19,15 +19,15 @@ public class RedisTestDataLoader {
 
     public void loadTestData() {
         HashOperations<String, String, Integer> hashOps = redisTemplate.opsForHash();
-        String redisKey = "SEARCH_COUNT";  // Redis 해시 키
+        String redisKey = "preference:search:count";  // Redis 해시 키
 
-        // ✅ 테스트 데이터 (userNo:itemId => searchCount)
+        // 테스트 데이터 (userNo:itemId => searchCount)
         Map<String, Integer> testData = Map.of(
-                "1:101", 5,
-                "1:102", 3,
-                "2:101", 7,
-                "3:103", 0,     // 검색 횟수 0인 경우도 테스트
-                "4:104", 12
+                "1:1", 5,
+                "1:2", 3,
+                "2:1", 7,
+                "2:13", 0,     // 검색 횟수 0인 경우도 테스트
+                "2:14", 12
         );
 
         // Redis에 데이터 삽입
