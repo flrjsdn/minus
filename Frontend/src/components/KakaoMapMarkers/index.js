f9d8af8b6780bff86494fe18306da07892e7df76
shLee <dltngh123470@gmail.com>
2025-02-13 16:38:56 +0900

    [FE/FIX] 배포 테스트 (#68)

--- Frontend/src/components/KakaoMapMarkers/index.js
+++ Frontend/src/components/KakaoMapMarkers/index.js
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { useKakaoMap } from "../../contexts/ KakaoMapContext";
 
 const KakaoMapMarkers = ({ storelist }) => {
